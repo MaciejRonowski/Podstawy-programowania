74c87b641554479825113ec1393b8b55015f95b0
MaciejRonowski <m.ronowski.969@studms.ug.edu.pl>
2025-03-03 16:14:57 +0100

    koniec zadania 3

---
+++ zadanie 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Podaj a=");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("a^3={0}", a * a * a);
+            Console.ReadKey(true); //pauza
+       
+        }
+    }
+}
