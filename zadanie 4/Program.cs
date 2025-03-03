d923f1d30413dc48c17bd195a857c02ff4042b1e
MaciejRonowski <m.ronowski.969@studms.ug.edu.pl>
2025-03-03 16:30:01 +0100

    koniec zadania 4

--- zadanie 4/Program.cs
+++ zadanie 4/Program.cs
@@ -12,9 +12,9 @@ namespace zadanie_4
         {
             double a, b;
             Console.Write("Podaj a=");
-            a = Convert.ToInt32(Console.ReadLine());
+            a = Convert.ToDouble(Console.ReadLine());
             Console.Write("Podaj b=");
-            b = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Pole={0}", a * b);
             Console.WriteLine("Obwód={0}", 2 * a + 2 * b);
             Console.ReadKey(true); //pauza
---
+++ zadanie 5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, h, l, V, Pc;
+            Console.Write
+        }
+    }
+}
