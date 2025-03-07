4ef94f6a29229d38874edc4602bfc95240fff290
MaciejRonowski <m.ronowski.969@studms.ug.edu.pl>
2025-03-07 14:29:24 +0100

    koniec zadania 1 oraz 2

---
+++ zadanie 1 warunki/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_1_warunki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Podaj liczbę całkowitą: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Liczba jest parzysta");
+            }
+            else
+                Console.WriteLine("Liczba jest nieparzysta");
+            Console.ReadKey(true); //pauza
+        }
+    }
+}
---
+++ zadanie 2 warunki/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_2_warunki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Podaj liczbę całkowitą: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a > 0)
+                Console.WriteLine("Liczba {0} jest dodatnią", a);
+            else if (a < 0)
+                Console.WriteLine("Liczba {0} jest ujemną", a);
+            else
+                Console.WriteLine("Liczba jest równa 0");
+            Console.ReadKey(true); //pauza
+
+        }
+    }
+}
