8a340be1c2aed0ecd87968e3af277d66e3790951
Aleksandra Stępkowska <a.stepkowska.664@studms.ug.edu.pl>
2025-04-14 13:46:21 +0200

    done

---
+++ zadanie 1 funkcje/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_1_funkcje
+{
+    internal class Program
+    {
+        static int suma(int a, int b)
+        { return a + b; }
+        static void Main(string[] args)
+        {
+            /*
+             Napisz funkcję o nazwie suma wyznaczającą sumę jej dwóch argumentów będących liczbami
+             całkowitymi. Napisz program wykorzystujący funkcję suma.
+             */
+            Console.WriteLine("2+5={0}", suma(2, 5));
+            Console.ReadKey(true);
+        }
+    }
+}
