5a4369b65037a3294aac41c3b12c0c7868632e6a
MaciejRonowski <m.ronowski.969@studms.ug.edu.pl>
2025-05-12 13:59:48 +0200

    done

---
+++ zadanie 5 funkcje/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_5_funkcje
+{
+    class Program
+    {
+        static int czy_pierwsza(int liczba)
+        {
+            int i;
+            for (i = 2; i < liczba / 2; i++)
+                if (liczba % i == 0)
+                    return 0;
+            return 1;
+        }
+        static void Main(string[] args)
+        {
+            /*
+             Napisz funkcję, która sprawdza czy jej argument jest liczbą pierwszą. Jako wartość funkcja
+             powinna zwracać 1 dla liczb pierwszych i 0 dla pozostałych liczb. Przetestuj działanie
+             utworzonej funkcji.
+             */
+            Console.WriteLine("Czy liczba 12 jest l. pierwszą: {0}", czy_pierwsza(12));
+            Console.WriteLine("Czy liczba 13 jest l. pierwszą: {0}", czy_pierwsza(13));
+            Console.ReadKey(true);
+        }
+    }
+}
